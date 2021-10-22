ae8fc63781b7cf0346b64300d819e4d0e131c7eb
Vukknesh <leomcn@hotmail.com>
2021-10-22 14:13:55 -0300

    a

--- userprofile/views.py
+++ userprofile/views.py
@@ -7,7 +7,6 @@ from .permissions import (
 )
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django_filters import rest_framework as filters
 from rest_framework.decorators import api_view
 from rest_framework.generics import (
     RetrieveUpdateAPIView,
@@ -18,45 +17,13 @@ from rest_framework.response import Response
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 
-import django_filters
 from django.db.models import Q
-
-
-class UserFilter(django_filters.FilterSet):
-    multi_name_fields = django_filters.CharFilter(
-        method='filter_by_all_name_fields')
-
-    class Meta:
-        model = User
-        fields = []
-
-    def filter_by_all_name_fields(self, queryset, name, value):
-        return queryset.filter(
-            Q(first_name__icontains=value) | Q(last_name__icontains=value) | Q(
-                username__icontains=value) | Q(email__icontains=value)
-        )
-
-
-class ProfileFilter(filters.FilterSet):
-    multi_name_fields = django_filters.CharFilter(
-        method='filter_by_all_name_fields')
-
-    class Meta:
-        model = Profile
-        fields = []
-
-    def filter_by_all_name_fields(self, queryset, name, value):
-        return queryset.filter(
-            Q(city__icontains=value) | Q(country__icontains=value) | Q(
-                state__icontains=value)
-        )
 
 
 class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all().order_by('first_name')
     serializer_class = UserSerializer
-    filterset_class = UserFilter
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsSameUserAllowEditionOrReadOnly,)
 
@@ -65,7 +32,6 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    filterset_class = ProfileFilter
 
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
