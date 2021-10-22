305f69c9164b78a88004fe1964e8c559137cb8cc
Vukknesh <leomcn@hotmail.com>
2021-10-22 13:42:06 -0300

    debug

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c+j_gsx_awz$w3nngrcl)$o(7wewv%q6(o!_(r!vxe&h&lxiq9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
